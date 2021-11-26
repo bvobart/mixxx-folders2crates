38310d8d2857d190b272e7be75d3adbe9389b73b
Bart van Oort <bart@vanoort.is>
2021-11-26 21:17:22 +0100

    Refactors crates.go to find CrateFolders instead of mixxxdb Crates, and adds a test

--- crates.go
+++ crates.go
@@ -7,17 +7,21 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/bvobart/mixxx-folders2crates/mixxxdb"
 	ignore "github.com/sabhiram/go-gitignore"
 )
 
-// FindCrates searches the given folder for music tracks that Mixxx can play.
+type CrateFolder struct {
+	Name   string
+	Tracks []TrackFile
+}
+
+// FindCrateFolders searches the given folder for music tracks that Mixxx can play.
 // When it finds a folder with at least track in it, it will make a crate with the name of the path to that folder
 // and the tracks that are directly inside the folder.
 // Respects the ignore patterns specified with the github.com/sabhiram/go-gitignore library.
 // Note: Tracks will not have any database IDs.
-func FindCrates(libfolder string, ignore *ignore.GitIgnore) ([]mixxxdb.CrateTracks, error) {
-	crates := []mixxxdb.CrateTracks{}
+func FindCrateFolders(libfolder string, ignore *ignore.GitIgnore) ([]CrateFolder, error) {
+	crates := []CrateFolder{}
 
 	err := filepath.WalkDir(libfolder, func(fpath string, d fs.DirEntry, err error) error {
 		if err != nil {
@@ -30,13 +34,13 @@ func FindCrates(libfolder string, ignore *ignore.GitIgnore) ([]mixxxdb.CrateTrac
 		}
 
 		// ignore folders from .crateignore
-		if ignore.MatchesPath(fpath) {
+		if ignore != nil && ignore.MatchesPath(fpath) {
 			return nil
 		}
 
 		// when encountering a non-ignored directory, find all tracks in it and make a crate
 		relpath := strings.TrimPrefix(fpath, libfolder+"/")
-		tracks, err := FindTracks(fpath, ignore)
+		tracks, err := FindTrackFiles(fpath, ignore)
 		if err != nil {
 			return fmt.Errorf("failed to find tracks in %s: %w", relpath, err)
 		}
@@ -46,9 +50,8 @@ func FindCrates(libfolder string, ignore *ignore.GitIgnore) ([]mixxxdb.CrateTrac
 			return nil
 		}
 
-		var id int64 // unknown at this point
 		name := NameCrate(relpath)
-		crates = append(crates, mixxxdb.NewCrateTracks(id, name, tracks))
+		crates = append(crates, CrateFolder{Name: name, Tracks: tracks})
 		return nil
 	})
 
---
+++ crates_test.go
@@ -0,0 +1,61 @@
+package folders2crates_test
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"testing"
+
+	folders2crates "github.com/bvobart/mixxx-folders2crates"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFindCrateFolders(t *testing.T) {
+	t.Run("Finds some crates in fabricated folder", func(t *testing.T) {
+		// Given:
+		dir, err := os.MkdirTemp("", "test-library")
+		require.NoError(t, err)
+
+		// TODO: crates are still temporarily having their name prefixed with TEST
+		expectedCrates := []folders2crates.CrateFolder{
+			{Name: "TEST - House", Tracks: []folders2crates.TrackFile{}},
+			{Name: "TEST - Techno", Tracks: []folders2crates.TrackFile{}},
+			// TODO: add subdirectories to test recursive searching behaviour
+		}
+
+		// ... a House folder with a load of .mp3 files in it
+		houseDir := path.Join(dir, "House")
+		require.NoError(t, os.Mkdir(houseDir, 0755))
+		for i := 0; i < 10; i++ {
+			fpath := path.Join(houseDir, fmt.Sprint("song-", i, ".mp3"))
+			f, err := os.Create(fpath)
+			require.NoError(t, err)
+			defer f.Close()
+
+			expectedCrates[0].Tracks = append(expectedCrates[0].Tracks, folders2crates.TrackFile(fpath))
+		}
+
+		// ... a Techno folder with a load of .flac files in it and a load of .cue files (which should be ignored)
+		technoDir := path.Join(dir, "Techno")
+		require.NoError(t, os.Mkdir(technoDir, 0755))
+		for i := 0; i < 10; i++ {
+			fpath := path.Join(technoDir, fmt.Sprint("song-", i, ".flac"))
+			f, err := os.Create(fpath)
+			require.NoError(t, err)
+			defer f.Close()
+
+			expectedCrates[1].Tracks = append(expectedCrates[1].Tracks, folders2crates.TrackFile(fpath))
+
+			f, err = os.Create(path.Join(houseDir, fmt.Sprint("cue-", i, ".cue")))
+			require.NoError(t, err)
+			defer f.Close()
+		}
+
+		// When:
+		crates, err := folders2crates.FindCrateFolders(dir, nil)
+
+		// Then:
+		require.NoError(t, err)
+		require.Equal(t, expectedCrates, crates)
+	})
+}
