bc3f9845fd746b95849ae7d7adbbc58b52b6ec12
Bart van Oort <bart@vanoort.is>
2021-11-26 21:20:20 +0100

    Refactors update.go according to new types

--- update.go
+++ update.go
@@ -3,6 +3,7 @@ package folders2crates
 import (
 	"errors"
 	"fmt"
+	"log"
 
 	"github.com/bvobart/mixxx-folders2crates/mixxxdb"
 	"github.com/hashicorp/go-multierror"
@@ -24,63 +25,81 @@ var ErrTrackNotFound = errors.New("track not found in MixxxDB")
 //   - Add all tracks that are in the generated crate.
 
 // UpdateCratesDB ...
-func UpdateCratesDB(db mixxxdb.MixxxDB, crates []mixxxdb.CrateTracks) error {
+func UpdateCratesDB(db mixxxdb.MixxxDB, crates []CrateFolder) error {
 	var errors *multierror.Error
 	for _, crate := range crates {
 		errors = multierror.Append(errors, UpdateCrateInDB(db, crate))
+		log.Println("Inserted crate: ", crate.Name)
 	}
+
 	return errors.ErrorOrNil()
 }
 
 // UpdateCrateInDB ...
-func UpdateCrateInDB(db mixxxdb.MixxxDB, crate mixxxdb.CrateTracks) error {
-	// check if all the crate's tracks are already inserted in Mixxx's DB.
-	// Simultaneously, get their IDs
-	var errors *multierror.Error
-	for i := range crate.Tracks {
-		errors = multierror.Append(errors, FindAndSetTrackID(db, &crate.Tracks[i]))
-	}
-	if err := errors.ErrorOrNil(); err != nil {
+func UpdateCrateInDB(db mixxxdb.MixxxDB, crate CrateFolder) error {
+	// find all the crate's tracks in Mixxx's DB.
+	trackIDs, err := FindTrackIDs(db, crate.Tracks)
+	if err != nil {
 		return err
 	}
 
-	// check whether the crate already exists
+	// find the crate in Mixxx's DB
 	dbcrate, err := db.Crates().FindByName(crate.Name)
 	if err != nil {
 		return fmt.Errorf("error finding crate '%s': %w", dbcrate.Name, err)
 	}
 
-	// if so, then wipe the crate, but save the crate's ID
-	if dbcrate != nil {
-		crate.ID = dbcrate.ID
+	// if it doesn't exist yet, create it
+	if dbcrate == nil {
+		dbcrate, err = db.Crates().InsertCrate(mixxxdb.Crate{Name: crate.Name})
+		if err != nil {
+			return fmt.Errorf("error inserting crate '%s': %w", crate.Name, err)
+		}
+	}
 
-		err := db.Crates().WipeTracks(dbcrate.ID)
+	// if it does, then remove all tracks in it
+	if dbcrate != nil {
+		err := db.CrateTracks().WipeCrate(dbcrate.ID)
 		if err != nil {
 			return fmt.Errorf("error wiping crate %d (%s): %w", dbcrate.ID, dbcrate.Name, err)
 		}
 	}
 
-	// then insert the crate and its tracks.
-	_, err = db.Crates().InsertTracks(crate)
+	// Now with an empty crate, we insert all tracks we want to have in there.
+	err = db.CrateTracks().InsertTracks(dbcrate.ID, trackIDs)
 	if err != nil {
-		return fmt.Errorf("error inserting crate '%s': %w", crate.Name, err)
+		return fmt.Errorf("error inserting tracks from crate '%s': %w", crate.Name, err)
 	}
 	return nil
 }
 
-// FindAndSetTrackID searches the MixxxDB for the given track by its path and, if found,
-// sets track.ID to the ID the track has in the DB.
-// Returns an error wrapping `ErrTrackNotFound`
-func FindAndSetTrackID(db mixxxdb.MixxxDB, track *mixxxdb.Track) error {
-	dbtrack, err := db.Tracks().FindByPath(track.Path)
-	if err != nil {
-		return fmt.Errorf("error finding track with path %s in Mixxx DB: %w", track.Path, err)
+// FindTrackIDs searches the DB to find the track IDs corresponding to the given track files.
+// Returns an error wrapping one or multiple `ErrTrackNotFound`s if there are tracks that cannot be found in Mixxx's DB.
+func FindTrackIDs(db mixxxdb.MixxxDB, tracks []TrackFile) ([]int, error) {
+	var errors *multierror.Error
+	trackIDs := make([]int, 0, len(tracks))
+
+	for _, fpath := range tracks {
+		trackLoc, err := db.TrackLocations().FindByLocation(string(fpath))
+		if trackLoc == nil {
+			err = fmt.Errorf("%s: %w", fpath, ErrTrackNotFound)
+		}
+		if err != nil {
+			errors = multierror.Append(errors, err)
+			continue
+		}
+
+		track, err := db.Tracks().FindByLocationID(trackLoc.ID)
+		if track == nil {
+			err = fmt.Errorf("%s: %w", fpath, ErrTrackNotFound)
+		}
+		if err != nil {
+			errors = multierror.Append(errors, err)
+			continue
+		}
+
+		trackIDs = append(trackIDs, track.ID)
 	}
 
-	if dbtrack == nil {
-		return fmt.Errorf("%s: %w", track.Path, ErrTrackNotFound)
-	}
-
-	track.ID = dbtrack.ID
-	return nil
+	return trackIDs, errors.ErrorOrNil()
 }
